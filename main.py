788ca4e9e440b17c761d31fc992406aa993bf270
NateDCoder <rubyandtwixie@gmail.com>
2024-06-19 14:26:44 -0400

    I dont know what im doing

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from sklearn import preprocessing
 
 import matplotlib.pyplot as plt
 import numpy as np
-from Network import *
-from sklearn.metrics import mean_absolute_error
 
 melbourne_file_path = './data/melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
@@ -13,45 +11,22 @@ melbourne_data = pd.read_csv(melbourne_file_path)
 melbourne_data = melbourne_data.dropna(axis=0)
 Price_Converter = np.max(melbourne_data.Price) - np.min(melbourne_data.Price)
 
-y = preprocessing.minmax_scale(melbourne_data.Price)
-
+y = melbourne_data.Price
+sorted_indexs = np.argsort(y)
 melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 'YearBuilt', 'Lattitude', 'Longtitude']
 
 
 scaler = preprocessing.StandardScaler()
-X = scaler.fit_transform(melbourne_data[melbourne_features])
-
-train_X, val_x, train_y, val_y = train_test_split(X, y, random_state=0)
-train_y = np.reshape(train_y, (-1,1))
-val_y = np.reshape(val_y, (-1,1))
-
-# Batch Size
-M = len(train_X)
-# M = 32
-
-model = Network([len(melbourne_features), 10, 1])
-data_errors = []
-test_errors = []
-EPOCHS = 1000
-for _ in range(EPOCHS):
-    # X is shape 4647 x 7
-    batch = np.random.randint(train_X.shape[0], size=M)
-    activation, Z = model.forward(train_X[batch, :].T)
-    test_prediction, _ = model.forward(val_x.T)
-    data_errors.append(mean_absolute_error(train_y[batch, :].T, activation[-1]))
-    test_errors.append(mean_absolute_error(val_y.T, test_prediction[-1]))
-    
-    print(mean_absolute_error(train_y[batch, :].T, activation[-1])*Price_Converter)
-    model.train(activation, Z, train_y[batch, :].T, M, train_X[batch, :])
-    # M+=1
-
+X = melbourne_data[melbourne_features]
+# X = scaler.fit_transform(melbourne_data[melbourne_features])
+print(X.shape)
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.plot(range(EPOCHS), data_errors, color='tab:orange', label='Train Error')
-ax.plot(range(EPOCHS), test_errors, color='tab:blue', label='Test Error')
+ax.scatter(np.sort(y),np.array(X['Landsize'])[sorted_indexs], color='tab:orange', label='Train Error')
+# ax.plot(range(EPOCHS), test_errors, color='tab:blue', label='Test Error')
 
-ax.set_xlabel('Epochs')
-ax.set_ylabel('Mean Absolute Error')
+ax.set_xlabel('Rooms')
+ax.set_ylabel('Price')
 ax.set_title('Training and Validation Error over Epochs')
 ax.legend()
 
