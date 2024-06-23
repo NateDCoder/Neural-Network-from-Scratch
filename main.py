95dcc88b5a977bcaba2e69abb0971322c54dc6b7
NateDCoder <rubyandtwixie@gmail.com>
2024-06-23 16:46:14 -0400

    Basic template down I think

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import numpy as np
 from Network import *
 from sklearn.metrics import mean_absolute_error
 
+import torch
+from torch import nn
+from torch.utils.data import TensorDataset, DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
 melbourne_file_path = './data/melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
 
@@ -21,29 +27,105 @@ melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 'YearBuil
 scaler = preprocessing.StandardScaler()
 X = scaler.fit_transform(melbourne_data[melbourne_features])
 
-train_X, val_x, train_y, val_y = train_test_split(X, y, random_state=0)
-train_y = np.reshape(train_y, (-1,1))
-val_y = np.reshape(val_y, (-1,1))
+train_X, test_x, train_y, test_y = train_test_split(X, y, random_state=0)
 
+train_data = TensorDataset(torch.tensor(train_X, dtype=torch.float32), torch.tensor(train_y, dtype=torch.float32))
+test_data = TensorDataset(torch.tensor(test_x, dtype=torch.float32), torch.tensor(test_y, dtype=torch.float32))
 # Batch Size
-M = len(train_X)
-# M = 32
+batch_size = 64
 
-model = Network([len(melbourne_features), 10, 1])
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(7, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+# Create data loaders.
+train_dataloader = DataLoader(train_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+print(f"Using {device} device")
+
+model = NeuralNetwork().to(device)
+print(model)
+
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        if len(X) < batch_size:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            break
+        X, y = X.to(device), y.to(device)
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+            
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {Price_Converter*test_loss:>8f} \n")
+
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
+
 data_errors = []
 test_errors = []
 EPOCHS = 1000
 for _ in range(EPOCHS):
     # X is shape 4647 x 7
-    batch = np.random.randint(train_X.shape[0], size=M)
-    activation, Z = model.forward(train_X[batch, :].T)
-    test_prediction, _ = model.forward(val_x.T)
-    data_errors.append(mean_absolute_error(train_y[batch, :].T, activation[-1]))
-    test_errors.append(mean_absolute_error(val_y.T, test_prediction[-1]))
+    # batch = np.random.randint(train_X.shape[0], size=M)
+    # activation, Z = model.forward(train_X[batch, :].T)
+    # test_prediction, _ = model.forward(val_x.T)
+    # data_errors.append(mean_absolute_error(train_y[batch, :].T, activation[-1]))
+    # test_errors.append(mean_absolute_error(val_y.T, test_prediction[-1]))
     
-    print(mean_absolute_error(train_y[batch, :].T, activation[-1])*Price_Converter)
-    model.train(activation, Z, train_y[batch, :].T, M, train_X[batch, :])
+    # print(mean_absolute_error(train_y[batch, :].T, activation[-1])*Price_Converter)
+    # model.train(activation, Z, train_y[batch, :].T, M, train_X[batch, :])
     # M+=1
+    pass
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
@@ -56,7 +138,6 @@ ax.set_title('Training and Validation Error over Epochs')
 ax.legend()
 
 plt.show()
-
 
 
 # 'Suburb', 'Address', 'Rooms', 'Type', 'Price', 'Method', 'SellerG',
